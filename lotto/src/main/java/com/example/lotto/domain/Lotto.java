95a5414540cfdd7ced5ef8722fe2603e335858cb
minnim1010 <pms1010123@daum.net>
2023-09-06 07:48:38 +0900

    feat(Lotto): Lotto 생성 시 예외 처리

--- lotto/src/main/java/com/example/lotto/domain/Lotto.java
+++ lotto/src/main/java/com/example/lotto/domain/Lotto.java
@@ -1,6 +1,9 @@
 package com.example.lotto.domain;
 
+import com.example.lotto.service.LottoProvider;
+
 import java.util.List;
+import java.util.Optional;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,7 +16,17 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(
-                String.format("현재 로또 번호 자릿수 %s: 로또 번호는 6자리입니다.", numbers.size()));
+                String.format("[Error] 현재 로또 번호 자릿수 %s: 로또 번호는 6자리입니다.", numbers.size()));
+        }
+
+        Optional<Integer> overRangeNum = numbers.stream()
+            .filter(num -> num < LottoProvider.LOTTO_MIN_NUMBER || LottoProvider.LOTTO_MAX_NUMBER < num)
+            .findFirst();
+        if (overRangeNum.isPresent()) {
+            throw new IllegalArgumentException(
+                String.format(
+                    "[Error] 유효하지 않은 로또 번호 %d: 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+                    , overRangeNum.get()));
         }
     }
 
---
+++ lotto/src/test/java/com/example/lotto/domain/LottoTest.java
@@ -0,0 +1,60 @@
+package com.example.lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTest {
+
+    @Nested
+    @DisplayName("새 로또를 생성할 때")
+    class CreateLotto {
+        @DisplayName("성공 시 새로운 로또를 생성한다.")
+        @Test
+        void createLotto() {
+            //given
+            List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+            //when
+            Lotto lotto = new Lotto(numbers);
+
+            //then
+            assertThat(lotto.getNumbers()).hasSize(6)
+                .isEqualTo(numbers);
+        }
+
+        @DisplayName("길이가 6이 아니면 예외가 발생한다.")
+        @Test
+        void failWithInvalidSize() {
+            //given
+            List<Integer> numbers1 = List.of(1, 2, 3, 4, 5, 6, 7);
+            List<Integer> numbers2 = List.of(1, 2, 3, 4);
+
+            //when then
+            assertThatThrownBy(() -> new Lotto(numbers1))
+                .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> new Lotto(numbers2))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("로또 번호가 1 이상 45 이하가 아니면 예외가 발생한다.")
+        @Test
+        void failWithOverRangeNumber() {
+            //given
+            List<Integer> numbers1 = List.of(1, 2, 3, 4, 5, 46);
+            List<Integer> numbers2 = List.of(0, 1, 2, 3, 4, 5);
+
+            //when then
+            assertThatThrownBy(() -> new Lotto(numbers1))
+                .isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> new Lotto(numbers2))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+}
