3c46a672a0a09f52758b6186b3e0db7b74d6ccfa
minnim1010 <pms1010123@daum.net>
2023-10-19 04:25:00 +0900

    feat: service 인터페이스 정의

---
+++ java-baseball-precourse/src/main/java/baseball/service/BaseballService.java
@@ -0,0 +1,10 @@
+package baseball.service;
+
+import baseball.domain.BaseballNumber;
+import baseball.domain.BaseballResult;
+
+public interface BaseballService {
+    BaseballNumber createRandomBaseballNumber();
+
+    BaseballResult calculateResult(BaseballNumber answer, BaseballNumber guess);
+}
